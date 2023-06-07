6d78490077b1365530008887240662c38e2a9f94
zackees <z@zackees.com>
2023-06-06 21:24:06 -0700

    adds run.py

---
+++ run.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+from PIL import Image
+import concurrent.futures
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+DOCS_DIR = os.path.join(HERE, "docs")
+OUT_DIR = os.path.join(HERE, "out")
+
+def get_all_files() -> list[str]:
+    """Get all files in the docs directory."""
+    files = []
+    for root, _, filenames in os.walk(DOCS_DIR):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
+
+def convert_png_to_webp(file: str, relative_path: str):
+    im = Image.open(file)
+    output_path = os.path.join(OUT_DIR, os.path.splitext(relative_path)[0] + '.webp')
+    print(f"Converting {relative_path} to {output_path}")
+    im.save(output_path, 'webp')
+
+files = get_all_files()
+
+shutil.rmtree(OUT_DIR, ignore_errors=True)
+os.makedirs(OUT_DIR, exist_ok=True)
+
+# Create a list to hold png conversion tasks
+png_conversion_tasks = []
+
+# Use a ProcessPoolExecutor to process png conversion in parallel
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    # Process each file
+    for file in files:
+        # Get the relative path of the file
+        relative_path = os.path.relpath(file, DOCS_DIR)
+        # Create the directory in the output directory
+        os.makedirs(os.path.join(OUT_DIR, os.path.dirname(relative_path)), exist_ok=True)
+        # Copy the file to the output directory
+        file_ext = os.path.splitext(file)[1]
+        if file_ext.lower() == ".png":
+            # Transform png with transparency into a webp image
+            task = executor.submit(convert_png_to_webp, file, relative_path)
+            png_conversion_tasks.append(task)
+        else:
+            shutil.copy(file, os.path.join(OUT_DIR, relative_path))
+
+    # Wait for all png conversions to complete
+    for task in concurrent.futures.as_completed(png_conversion_tasks):
+        pass
